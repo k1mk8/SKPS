f26f7343314af1d6a3ca17ad54477be3594671f4
kkasper1 <karol.kasperek@pw.edu.pl>
2022-03-23 18:34:27 +0100

    zad2

--- cw2/gpio_led_2.py
+++ cw2/gpio_led_2.py
@@ -1,17 +1,9 @@
-from cmath import sin
-import gpio4
+from periphery import PWM
+fromt math import sin, pi
 from time import sleep
-from math import sin, pi
 
-gpio27 = gpio4.SysfsGPIO(27)
-gpio27.export = True # use the pin
-gpio27.direction = 'out' # set direction to output
-duty_cycle = 0.0
-for i in range(10):
-    gpio27.value = 1 # set value to high
-    x = 1 * duty_cycle
-    sleep(1*duty_cycle)
-    gpio27.value = 0 # set value to low
-    sleep(1-x)
-    duty_cycle = sin((pi/6)*i)
-gpio27.export = False # cleanup
+pwm = PWM(0, 10)
+
+for i in range(50):
+    pwm.duty_cycle = abs(sin(pi/12)*i)
+    sleep(0.2)
